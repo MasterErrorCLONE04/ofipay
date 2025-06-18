6f9a40c5d67fb907b5f13bb67789f363671e5557
David Velasquez Perdomo MasterErrorCLONE04 <168290389+MasterErrorCLONE04@users.noreply.github.com>
2025-06-17 20:46:36 -0500

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Search, Calendar, User, Bell } from 'lucide-react-native';
+import { Chrome as Home, Search, Calendar, User, Bell } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
